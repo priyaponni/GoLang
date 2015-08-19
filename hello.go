97b2f8a94b66c6c2160a3eac009ee1423253696c
Ponni Priyadharshni <priyaponni@gmail.com>
2015-08-19 14:24:52 -0700

    Random Number

--- hello.go
+++ hello.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 	)
 
 func main() {
     fmt.Printf("Hello, Dharshni Ramkumar!\n")
 	fmt.Printf("Welcome to Go Language\n")
 	fmt.Println("The time is -- ", time.Now())
+	fmt.Println("Random Number -- ", rand.Intn(10))
 }
