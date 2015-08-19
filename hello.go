822f512c08da3a2b58185f620663ca175b2b70ba
Ponni Priyadharshni <priyaponni@gmail.com>
2015-08-19 14:12:00 -0700

    Adding time

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	)
 
 func main() {
     fmt.Printf("Hello, Dharshni Ramkumar!\n")
-	fmt.Printf("Welcome to Go Language")
+	fmt.Printf("Welcome to Go Language\n")
+	fmt.Println("The time is", time.Now())
 }
